c77bcaea9c8148c74d4d3d83b317650e87141d74
stefanghadjiew <stefanghadjiew@abv.bg>
2020-10-02 23:15:55 +0300

    Updates

---
+++ precache-manifest.cbdaea8baed8561995d9952f38480797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17303579b0e93a9e6e2cde83cd98257",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "83148744dcee24434030",
+    "url": "/weather-app/static/css/main.0ecdfb48.chunk.css"
+  },
+  {
+    "revision": "744b52bafb83f537fd6a",
+    "url": "/weather-app/static/js/2.9da634f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.9da634f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83148744dcee24434030",
+    "url": "/weather-app/static/js/main.b564c6ac.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "df65651bbe6711a0507a3ab52a837b56",
+    "url": "/weather-app/static/media/background1.df65651b.jpg"
+  }
+]);
